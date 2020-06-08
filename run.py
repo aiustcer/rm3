6070c7970c82e92c77ef51a74cae590a5dc8948d
aiustcer <31919111+aiustcer@users.noreply.github.com>
2020-06-08 13:08:08 +0800

    1

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ Options:
     --seed=<int>                            seed [default: 0]
     --batch-size=<int>                      batch size [default: 32]
     --embed-size=<int>                      embedding size [default: 256]
-    --hidden-size=<int>                     hidden size [default: 350]
+    --hidden-size=<int>                     hidden size [default: 256]
     --clip-grad=<float>                     gradient clipping [default: 5.0]
     --log-every=<int>                       log every [default: 10]
     --max-epoch=<int>                       max epoch [default: 30]
