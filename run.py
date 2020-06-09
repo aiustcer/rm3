bd8526351d63cb8d58f7d4f9e39aa504894fbe36
aiustcer <31919111+aiustcer@users.noreply.github.com>
2020-06-09 23:52:28 +0800

    1

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ Options:
     --dev-tgt=<file>                        dev target file
     --vocab=<file>                          vocab file
     --seed=<int>                            seed [default: 0]
-    --batch-size=<int>                      batch size [default: 300]
+    --batch-size=<int>                      batch size [default: 256]
     --embed-size=<int>                      embedding size [default: 256]
     --hidden-size=<int>                     hidden size [default: 256]
     --clip-grad=<float>                     gradient clipping [default: 5.0]
